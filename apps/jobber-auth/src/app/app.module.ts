6014fb3bb0da7f6529d56c36701f241b62470e09
HaoYu1 <hao_yu@ao.uscourts.gov>
2026-02-11 15:42:56 -0500

    works on JWT auth

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AuthModule } from './auth/auth.module';
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      context: ({ req, res }) => ({ req, res }),
       autoSchemaFile: true,
     }),
     UsersModule,
--- apps/jobber-auth/src/app/auth/auth.module.ts
+++ apps/jobber-auth/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
   ],
   providers: [AuthResolver, AuthService],
 })
